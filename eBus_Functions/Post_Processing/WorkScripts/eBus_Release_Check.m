70af0ad0079813711eee674c4c87ccd0d83ce94e
hvpn25-Coder <hvpn25@gmail.com>
2026-03-19 06:07:35 +0530

    Add eBus release check menu entry

---
+++ eBus_Functions/Post_Processing/WorkScripts/eBus_Release_Check.m
@@ -0,0 +1,6 @@
+function eBus_Release_Check()
+% Compare eBus configuration releases and validate simulation file parity.
+fprintf('eBus_Release_Check is not implemented yet.\n');
+fprintf(['Goal: compare eBus configuration releases and verify both release folders ', ...
+    'contain the same number of similarly named simulation MAT files.\n']);
+end
