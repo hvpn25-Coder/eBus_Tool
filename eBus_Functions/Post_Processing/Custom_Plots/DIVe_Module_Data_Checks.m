0467bcc0532609d4d534675bde48516b99299d98
hvpn25-Coder <hvpn25@gmail.com>
2026-04-10 15:48:54 +0530

    Update DIVe_Module_Data_Checks.m

--- eBus_Functions/Post_Processing/Custom_Plots/DIVe_Module_Data_Checks.m
+++ eBus_Functions/Post_Processing/Custom_Plots/DIVe_Module_Data_Checks.m
@@ -2,13 +2,13 @@
 
 MS = sMP.cfg.Configuration.ModuleSetup;
 nM = numel(MS); nF = 6;
-nDS = max(arrayfun(@(x)numel(x.DataSet),MS)) ;
+nDS = max(arrayfun(@(x)numel(x.DataSet),MS));
 
 FN = strings(nF,nM); FV = FN;
-DVN = cell (nDS,nM) ; DCN = DVN;
+DVN = cell(nDS,nM); DCN = DVN;
 
-for m=l:nM
-    f = string (fieldnames (MS (m) .Module)); v = string(struct2cell (MS (m) .Module));
+for m=1:nM
+    f = string(fieldnames(MS(m).Module)); v = string(struct2cell(MS(m).Module));
     k = 1:min(nF,numel(f)); FN(k,m)=f(k); FV(k,m)=v(k);
     ds = MS (m).DataSet;
     if ~isempty(ds)
@@ -18,15 +18,15 @@ for m=l:nM
 end 
 
 CAT = FV(2,:);
-DN = strings(size(DVN)); DN(cellfun(@(x)ischar(x)||isstring(x),DVN)) = string(DVN(cellfun(@(x)ischar(x)||isstring(x),DVN)));
-DV = strings(size(DCN)); DV(cellfun(@(x)ischar(x)||isstring(x),DCN)) = string(DCN(cellfun(@(x)ischar(x)||isstring(x),DCN)));
+DN = strings(size(DVN));  DN(cellfun(@(x)ischar(x)||isstring(x),DVN)) = string(DVN(cellfun(@(x)ischar(x)||isstring(x),DVN)));
+DV = strings(size(DCN));  DV(cellfun(@(x)ischar(x)||isstring(x),DCN)) = string(DCN(cellfun(@(x)ischar(x)||isstring(x),DCN)));
 
 DIVe_Mod_Data = struct; 
 
 for k=1:nM
-    c = CAT (k);
+    c = CAT(k);
     DIVe_Mod_Data.(c+"_a") = join(FN(:,k),newline);
-    DIVe_Mod_Data.(c+"_b") = join(FV(:,k),newline) ;
+    DIVe_Mod_Data.(c+"_b") = join(FV(:,k),newline);
     DIVe_Mod_Data.(c+"_c") = join(DV(DV(:,k)~="",k),newline);
     DIVe_Mod_Data.(c+"_d") = join(DN(DN(:,k)~="",k),newline);
 end
@@ -34,7 +34,7 @@ end
 %% Ordering
 ord = ["bdry","human","phys","ctrl"];
 [~,g] = ismember(FV(1,:),ord);
-idx = find(g); [~,o]=sort (g(idx)); idx=idx(o);
+idx = find(g); [~,o]=sort(g(idx)); idx=idx(o);
 
 FN=FN(:,idx); FV=FV(:,idx); DCN=DCN(:,idx); DVN=DVN(:,idx);
 
@@ -45,18 +45,17 @@ blk = { "DB","bdry",NaN
         "DP","phys",10
         "DC","ctrl",20 };
 
-for b = 1:size (blk, 1)
+for b = 1:size(blk,1)
     A = FV(2:4, FV(1,:)==blk{b,2});
     if isempty(A), continue, end
-    
-    A = string (A);
+    A = string(A);
     maxI = blk{b,3};
 
     if ~isnan(maxI) && size(A,2) < maxI
-        A (:,end+1:maxI) = " ";
+        A(:,end+1:maxI) = " ";
     end
 
-    for i = 1:size (A,2)
+    for i = 1:size(A,2)
         for j = 1:size(A,1)
             DIVe_Mod_Data.(blk{b,1}+"_"+j+"_"+i) = A(j,i);
         end
