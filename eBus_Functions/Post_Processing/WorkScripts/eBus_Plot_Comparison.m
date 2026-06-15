5966cd734bcfda929b11ac1f9268fca1e7c27c03
hvpn25-Coder <hvpn25@gmail.com>
2026-06-16 03:56:26 +0530

    Add eBus plot comparison utility link

---
+++ eBus_Functions/Post_Processing/WorkScripts/eBus_Plot_Comparison.m
@@ -0,0 +1,5 @@
+function eBus_Plot_Comparison()
+% Compare plots from two simulation or measurement files.
+fprintf('eBus_Plot_Comparison is not implemented yet.\n');
+fprintf('Purpose: compare the plots of two simulation or measurement files.\n');
+end
